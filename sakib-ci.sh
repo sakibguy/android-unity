3df8de5628ccaaa9ef505e0c6556bfacd7943470
sakib-rahman-bangladesh <sakib.rahman.0000@gmail.com>
2020-12-03 10:56:48 +0600

    obstacle 26: HOLYJAPAN

--- sakib-ci.sh
+++ sakib-ci.sh
@@ -66,7 +66,7 @@ echo
 ## Default: Commit skipped|forgotten
 ## new-iop-apps: [ok] classify 10k images
 
-git commit -m "obstacle 25: SPINNINGLEPRECHAUN"
+git commit -m "obstacle 26: HOLYJAPAN"
 
 ############
 # Step: 05 #
