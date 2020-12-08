a61e7d32676dcb8d513f5d998bff7706c8545ec9
sakib-rahman-bangladesh <sakib.rahman.0000@gmail.com>
2020-12-08 12:46:43 +0600

    obstacle 27: MYANDOTS

--- sakib-ci.sh
+++ sakib-ci.sh
@@ -66,7 +66,7 @@ echo
 ## Default: Commit skipped|forgotten
 ## new-iop-apps: [ok] classify 10k images
 
-git commit -m "obstacle 25: SPINNINGLEPRECHAUN"
+git commit -m "obstacle 27: MYANDOTS"
 
 ############
 # Step: 05 #
