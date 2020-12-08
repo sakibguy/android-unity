c0c68bc489ddcc48a407e2866e18959dd8f0a2f4
sakib-rahman-bangladesh <sakib.rahman.0000@gmail.com>
2020-12-08 12:47:38 +0600

    obstacle 28: BOXJUMPUP

--- sakib-ci.sh
+++ sakib-ci.sh
@@ -66,7 +66,7 @@ echo
 ## Default: Commit skipped|forgotten
 ## new-iop-apps: [ok] classify 10k images
 
-git commit -m "obstacle 27: MYANDOTS"
+git commit -m "obstacle 28: BOXJUMPUP"
 
 ############
 # Step: 05 #
