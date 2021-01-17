b1aa2536510aa39eb086e92a082c5373bc651c73
sakib-rahman-bangladesh <sakib.rahman.0000@gmail.com>
2021-01-17 19:30:06 +0600

    obstacle 44: BIGKENO

--- sakib-ci.sh
+++ sakib-ci.sh
@@ -66,7 +66,7 @@ echo
 ## Default: Commit skipped|forgotten
 ## new-iop-apps: [ok] classify 10k images
 
-git commit -m "obstacle 43: BIGLEPRECHAUN"
+git commit -m "obstacle 44: BIGKENO"
 
 ############
 # Step: 05 #
