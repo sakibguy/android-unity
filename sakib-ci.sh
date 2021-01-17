ef5bd25585886b5a10622627689f756bf3b2ec4a
sakib-rahman-bangladesh <sakib.rahman.0000@gmail.com>
2021-01-17 19:05:14 +0600

    obstacle 36: SHEEPOP

--- sakib-ci.sh
+++ sakib-ci.sh
@@ -66,7 +66,7 @@ echo
 ## Default: Commit skipped|forgotten
 ## new-iop-apps: [ok] classify 10k images
 
-git commit -m "obstacle 35: EGYPTIANKENO"
+git commit -m "obstacle 36: SHEEPOP"
 
 ############
 # Step: 05 #
