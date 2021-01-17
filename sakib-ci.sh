61f6583d8b955daccbe69b13164869ac23f064c1
sakib-rahman-bangladesh <sakib.rahman.0000@gmail.com>
2021-01-17 19:14:41 +0600

    obstacle 40: MAYANKENO

--- sakib-ci.sh
+++ sakib-ci.sh
@@ -66,7 +66,7 @@ echo
 ## Default: Commit skipped|forgotten
 ## new-iop-apps: [ok] classify 10k images
 
-git commit -m "obstacle 39: MOVINGUP"
+git commit -m "obstacle 40: MAYANKENO"
 
 ############
 # Step: 05 #
