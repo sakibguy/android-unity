20e7d764ed4b9febbff8a957f28a69fba792448d
sakib-rahman-bangladesh <sakib.rahman.0000@gmail.com>
2021-01-17 19:03:32 +0600

    obstacle 35: EGYPTIANKENO

--- sakib-ci.sh
+++ sakib-ci.sh
@@ -66,7 +66,7 @@ echo
 ## Default: Commit skipped|forgotten
 ## new-iop-apps: [ok] classify 10k images
 
-git commit -m "obstacle 34: SPRINGPANDA"
+git commit -m "obstacle 35: EGYPTIANKENO"
 
 ############
 # Step: 05 #
