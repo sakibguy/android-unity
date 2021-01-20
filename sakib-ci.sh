827e7cfa2c3915fd324f75c51ded968e3093344f
sakib-rahman-bangladesh <sakib.rahman.0000@gmail.com>
2021-01-20 18:35:24 +0600

    obstacle 45: BIGEGYPTIAN

--- sakib-ci.sh
+++ sakib-ci.sh
@@ -66,7 +66,7 @@ echo
 ## Default: Commit skipped|forgotten
 ## new-iop-apps: [ok] classify 10k images
 
-git commit -m "obstacle 44: BIGKENO"
+git commit -m "obstacle 45: BIGEGYPTIAN"
 
 ############
 # Step: 05 #
