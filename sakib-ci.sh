6fd03ffd870b6213b3102eb773077733527735ea
sakib-rahman-bangladesh <sakib.rahman.0000@gmail.com>
2021-03-30 09:01:18 +0600

    obstacle 49: DONKEYSHOT

--- sakib-ci.sh
+++ sakib-ci.sh
@@ -66,7 +66,7 @@ echo
 ## Default: Commit skipped|forgotten
 ## new-iop-apps: [ok] classify 10k images
 
-git commit -m "obstacle 48: COUNTINGSTAR"
+git commit -m "obstacle 49: DONKEYSHOT"
 
 ############
 # Step: 05 #
