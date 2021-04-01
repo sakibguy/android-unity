22daeb75cafc7104f263b701b29599a7e55b0f15
sakib-rahman-bangladesh <sakib.rahman.0000@gmail.com>
2021-04-01 21:02:48 +0600

    obstacle 50: BALLGRINDER

--- sakib-ci.sh
+++ sakib-ci.sh
@@ -66,7 +66,7 @@ echo
 ## Default: Commit skipped|forgotten
 ## new-iop-apps: [ok] classify 10k images
 
-git commit -m "obstacle 49: DONKEYSHOT"
+git commit -m "obstacle 50: BALLGRINDER"
 
 ############
 # Step: 05 #
