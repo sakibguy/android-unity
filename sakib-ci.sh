8078020ad046e003d4064c159759792a3f11c199
sakib-rahman-bangladesh <sakib.rahman.0000@gmail.com>
2021-04-03 12:44:07 +0600

    obstacle 51: COUNTRYBALL

--- sakib-ci.sh
+++ sakib-ci.sh
@@ -66,7 +66,7 @@ echo
 ## Default: Commit skipped|forgotten
 ## new-iop-apps: [ok] classify 10k images
 
-git commit -m "obstacle 50: BALLGRINDER"
+git commit -m "obstacle 51: COUNTRYBALL"
 
 ############
 # Step: 05 #
