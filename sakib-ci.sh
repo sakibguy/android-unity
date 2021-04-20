56edb139e00e556a0fb511aa35a3a942e7a9a732
sakib-rahman-bangladesh <sakib.rahman.0000@gmail.com>
2021-04-20 21:43:36 +0600

    obstacle 62: FOOTBALLSTAR

--- sakib-ci.sh
+++ sakib-ci.sh
@@ -66,7 +66,7 @@ echo
 ## Default: Commit skipped|forgotten
 ## new-iop-apps: [ok] classify 10k images
 
-git commit -m "obstacle 61: FOOTBALLCHALLANGE"
+git commit -m "obstacle 62: FOOTBALLSTAR"
 
 ############
 # Step: 05 #
